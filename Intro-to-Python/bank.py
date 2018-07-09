e260158e2d8fe1c657372465d91d50db5d699140
Caleb Kipkurui <calebkibet88@gmail.com>
2018-07-09 21:53:25 +0300

    Updates after the class

---
+++ Intro-to-Python/bank.py
@@ -0,0 +1,22 @@
+acountbal = 50000
+choice = input("Please enter 'b' to check balance or 'w' to withdraw: ")
+while choice != 'q':
+    if choice.lower() in ('w','b'):
+        if choice.lower() == 'b':
+            print("Your balance is: %d" % acountbal)
+            print("Anything else?")
+            choice = input("Enter b for balance, w to withdraw or q to quit 1: ")
+            print(choice.lower())
+        else:
+            withdraw = float(input("Enter amount to withdraw: "))
+            if withdraw <= acountbal:
+                print("here is your: %.2f" % withdraw)
+                acountbal = acountbal - withdraw
+                print("Anything else?")
+                choice = input("Enter b for balance, w to withdraw or q to quit 2: ")
+                #choice = 'q'
+            else:
+                print("You have insufficient funds: %.2f" % acountbal)
+    else:
+        print("Wrong choice!")
+        choice = input("Please enter 'b' to check balance or 'w' to withdraw: ")
